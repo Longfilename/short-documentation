6691e7eea9e777df76e5f3b512507ce6998d6c91
Longfilename <longfilename@gmail.com>
2015-06-12 21:07:00 -0400

    changed file types to a SELECT.

--- src/pages/_documentation/page.js
+++ src/pages/_documentation/page.js
@@ -1,7 +1,9 @@
 jQuery(function ($) {
     var $files = $("select.files"),
-        $tabs = $("div.tabs"),
-        $body = $("div.tab-body");
+        $fileTypes = $("select.file-type"),
+        $body = $("div.tab-body"),
+        // the data attribute where myData is stored;
+        keyName = "files";
     
     // on change, grab the contents of the URL and display them;
     $files.on("change", function () {
@@ -29,60 +31,60 @@ jQuery(function ($) {
             });
     });
     
+    $fileTypes.on("change", function () {
+        var $this = $(this).find("option:selected"),
+            tabData = $this.data();
+        
+        // kill any existing content;
+        $files.empty();
+        
+        // and for each file in this tab...;
+        // iterate through the array of files;
+        tabData[keyName].map(function (file) {
+            // and create an OPTION for the SELECT;
+            $files.append("<option>" + tabData.folder + file + "</option>");
+        });
+    
+        // now that we're done adding all the values, trigger a change;
+        $files.trigger("change");
+    });
+    
     // on click of navigation links;
     $("select.objects").on("change", function () {
-        var $option = $(this).find("option:selected"),
-            data = $option.data(),
-            $tab;
+        var $this = $(this).find("option:selected"),
+            data = $this.data(),
+            $option;
         
         // load this page or module;
         $("iframe").prop("src", data.path);
         
         // clear any previous files and tabs;
         $files.empty();
-        $tabs.empty();
+        $fileTypes.empty();
         
         // and add new tabs (and options in the select);
         Object.keys(data).map(function (key) {
             // don't do anything with the folder name;
             if (typeof data[key] !== "string") {
                     // content to pass on when this new A is clicked;
-                var myData = data[key],
-                    // the data attribute where myData is stored;
-                    keyName = "files";
+                var myData = data[key];
                 
                 // if we have actual content for this file type;
                 if (myData.length) {
                     // create the tab link;
-                    $tab = $("<a/>")
+                    $option = $("<option/>")
                         .html(key)
                         .data(keyName, myData)
-                        .on("click", function () {
-                            var $this = $(this),
-                                tabData = $this.data();
-                            
-                            // kill any existing content;
-                            $files.empty();
-                            
-                            // and for each file in this tab...;
-                            // iterate through the array of files;
-                            tabData[keyName].map(function (file) {
-                                // and create an OPTION for the SELECT;
-                                $files.append("<option>" + data.folder + file + "</option>");
-                            });
-                            
-                            // now that we're done adding all the values, trigger a change;
-                            $files.trigger("change");
-                        });
+                        .data("folder", data.folder);
                     
                     // then add this tab to the page;
-                    $tabs.append($tab);
+                    $fileTypes.append($option);
                 }
             }
         });
         
         // we're done adding all the tab links, click the first one (so we have content to start with);
-        $tabs.find("a:first-child").trigger("click");
+        $fileTypes.trigger("change");
     });
     
     // now click the first item just to get things going;
