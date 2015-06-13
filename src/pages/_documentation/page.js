8524032fe3d3af021c08952c29647f96776b3e03
Longfilename <longfilename@gmail.com>
2015-06-13 11:31:56 -0400

    refactored doc JS to not use many data attributes.

--- src/pages/_documentation/page.js
+++ src/pages/_documentation/page.js
@@ -1,3 +1,6 @@
+var ShortDocs = window.ShortDocs || {},
+    hljs = window.hljs;
+
 jQuery(function ($) {
     var $h1 = $("h1"),
         $h2 = $("h2"),
@@ -6,13 +9,92 @@ jQuery(function ($) {
         $bodyCode = $("code.content"),
         $bodyDiv = $("div.content");
     
-    $("a.set-flex-width").on("click", function () {
-        var $a = $(this),
-            data = $a.data();
+    // build the modules / pages SELECT;
+    $objects
+        .each(function () {
+            var $select = $(this),
+                buildOptions = function (data, title) {
+                    var $optgroup, $option;
+                    
+                    // loop through the data and build the OPTIONS;
+                    // if there is data;
+                    if (data.length) {
+                        // create the OPTGROUP (pages or modules);
+                        $optgroup = $("<optgroup label='" + title + "'/>");
+                        
+                        // loop through each group data and build an OPTION per file;
+                        data.map(function (item) {
+                            $option = $("<option />");
+                            
+                            // need to make some adjustments to the data;
+                            // don't want these parts of the path during Jade processing;
+                            item.path =  "./html/" + item.page;
+                            item.folder = "./" + item.folder;
+                            
+                            // build the OPTION;
+                            $option
+                                .html(item.title)
+                                // attach the data to this OPTION so when we can access it onChange;
+                                .data("config", item);
+                            
+                            // put the OPTION in the OPTGROUP;
+                            $optgroup.append($option);
+                        });
+                    }
+                    
+                    // return our newly created OPTGROUP (full of OPTIONS);
+                    return ($optgroup) ? $optgroup : "";
+                };
+            
+            // build an OPTIONS for the pages;
+            $select.append(buildOptions(ShortDocs.data.pages, "Pages"));
+            // then the modules;
+            $select.append(buildOptions(ShortDocs.data.modules, "Modules"));
+        })
+        .on("change", function () {
+            var $option = $(this).find("option:selected"),
+                config = $option.data().config;
+            
+            // update the label;
+            $h1.find("span").text($objects.val());
         
-        $("div.col-iframe").css("flex", data.widths[0]);
-        $("div.col-readme").css("flex", data.widths[1]);
-    });
+            // load this page or module;
+            $("iframe").prop("src", config.path);
+        
+            // clear any previous files;
+            $files.empty();
+        
+            // and add new OPTIONs in the files SELECT;
+            Object.keys(config).map(function (key) {
+                // don't do anything with the folder name;
+                if (typeof config[key] !== "string") {
+                    var keyData = config[key],
+                        // each type of content gets a OPTGROUP (md, jade, scss, json, js);
+                        $optgroup = $("<optgroup label='" + key + "' />"),
+                        // each file gets an OPTION;
+                        $option;
+                
+                    // loop through the files (if there are any);
+                    keyData.length && keyData.map(function (file) {
+                        // create an OPTION;
+                        $option = $("<option />");
+                        $option.html(config.folder + file);
+                        // and add it to this OPTGROUP;
+                        $optgroup.append($option);
+                    });
+                
+                    // once all the files are added for this group;
+                    // add the OPTGROUP to the SELECT;
+                    // if it has content;
+                    if ($optgroup.children().size()) {
+                        $files.append($optgroup);
+                    }
+                }
+            });
+        
+            // we're done adding all the tab links, click the first one (so we have content to start with);
+            $files.trigger("change");
+        });
     
     // on change, grab the contents of the URL and display them;
     $files.on("change", function () {
@@ -32,8 +114,8 @@ jQuery(function ($) {
                         
                         // if this is a JSON file, we need to stringify the response;
                         if (urlType === "json") {
-                            content = JSON.stringify(data);
-                        // JS files come back as an object;
+                            content = JSON.stringify(data, null, "\t");
+                            // JS files come back as an object;
                         } else if (urlType === "js") {
                             content = data.responseText;
                         }
@@ -63,52 +145,14 @@ jQuery(function ($) {
             });
     });
     
-    // on click of navigation links;
-    $objects.on("change", function () {
-        var $this = $(this).find("option:selected"),
-            data = $this.data();
-    
-        // update the label;
-        $h1.find("span").text($objects.val());
-        
-        // load this page or module;
-        $("iframe").prop("src", data.path);
-        
-        // clear any previous files;
-        $files.empty();
-        
-        // and add new OPTIONs in the files SELECT;
-        Object.keys(data).map(function (key) {
-            // don't do anything with the folder name;
-            if (typeof data[key] !== "string") {
-                var myData = data[key],
-                    // each type of content gets a OPTGROUP;
-                    $optgroup = $("<optgroup label='" + key + "' />"),
-                    // each file gets an OPTION;
-                    $option;
-                
-                // loop through the files;
-                myData.length && myData.map(function (file) {
-                    // create an OPTION;
-                    $option = $("<option />");
-                    $option.html(data.folder + file);
-                    // and add it to this OPTGROUP;
-                    $optgroup.append($option);
-                });
-                
-                // once all the files are added for this group;
-                // add the OPTGROUP to the SELECT;
-                // if it has content;
-                if ($optgroup.children().size()) {
-                    $files.append($optgroup);
-                }
-            }
-        });
-        
-        // we're done adding all the tab links, click the first one (so we have content to start with);
-        $files.trigger("change");
-    });
-    
     // now click the first item just to get things going;
     $objects.trigger("change");
+    
+    $("a.set-flex-width").on("click", function () {
+        var $a = $(this),
+            data = $a.data();
+        
+        $("div.col-iframe").css("flex", data.widths[0]);
+        $("div.col-readme").css("flex", data.widths[1]);
+    });
 });
