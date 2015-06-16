862d0625805430696f8f06b36f3a2253b69db55a
Longfilename <longfilename@gmail.com>
2015-06-16 10:28:51 -0400

    added CSS to every page.

---
+++ src/pages/contact-form/page.js
@@ -0,0 +1,4 @@
+require("../../modules/footer/module");
+require("../../modules/header/module");
+require("../../modules/list/ajax/module");
+require("../../modules/list/templated/module");
---
+++ src/pages/search-results/readme.md
@@ -0,0 +1,3 @@
+# Home Page
+
+Only the most important content bubbles up to the home page.
