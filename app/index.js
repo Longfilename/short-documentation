bab06c30d3eeb6afa1114f202bf4efab761ab87d
Longfilename <longfilename@gmail.com>
2015-07-07 17:15:31 -0400

    removed gitignore.

--- app/index.js
+++ app/index.js
@@ -157,7 +157,6 @@ module.exports = yeoman.generators.Base.extend({
         
         // editor and gulp plugin configurations, nothing to serve to the client browser;
         this.copy(".eslintrc",      ".eslintrc");          // JS lint config (for gulp:js);
-        this.copy(".gitignore",     ".gitignore");         // tell git what files to not track;
         this.copy(".scss-lint.yml", ".scss-lint.yml");     // SCSS lint config (for gulp:scss);
         this.copy("gulpfile.js",    "gulpfile.js");        // gulp loader (all tasks are defined in /gulp);
         this.copy("package.json",   "package.json");       // npm config;
