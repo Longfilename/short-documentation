cee0440d016e13066805734111b6fa5252ce6799
Longfilename <longfilename@gmail.com>
2016-11-01 17:53:54 -0400

    resizer is now functional.

--- app/templates/src/docs/js/_resizer.js
+++ app/templates/src/docs/js/_resizer.js
@@ -1,12 +1,26 @@
 jQuery(($) => {
   $('.resizer').each((index, resizer) => {
     const $resizer = $(resizer);
-    const $view = $resizer.prev();
-    const $iframeContainer = $view.find('.component__view-back');
-    const $iframe = $view.find('.component__view-iframe');
-    const resizedClass = 'component__view-back--resized';
+    // const $view = $resizer.prev();
+    // const $iframeContainer = $view.find('.component__view-back');
+    // const $iframe = $view.find('.component__view-iframe');
+    // const resizedClass = 'component__view-back--resized';
     const activeClass = 'resizer-link--active';
     const $links = $resizer.find('.resizer-link');
+
+    let $target;
+    let $iframe;
+    let resizedClass;
+
+    if ($resizer.hasClass('resizer--template')) {
+      $target = $('.template__iframe-wrapper');
+      $iframe = $('.template__iframe');
+      resizedClass = 'template__iframe-wrapper--resized';
+    } else {
+      $target = $resizer.prev().find('.component__view-back');
+      $iframe = $target.find('.component__view-iframe');
+      resizedClass = 'component__view-back--resized';
+    }
 
     $links.each((i, link) => {
       const $link = $(link);
@@ -14,18 +28,18 @@ jQuery(($) => {
 
       $link.on('click.resizer', () => {
         if (dataResizer === '100%') {
-          $iframeContainer.removeClass(resizedClass);
+          $target.removeClass(resizedClass);
         } else {
-          $iframeContainer.addClass(resizedClass);
+          $target.addClass(resizedClass);
         }
 
-        $iframeContainer.one('transitionend.resizer', () => {
+        $target.one('transitionend.resizer', () => {
           setTimeout(() => {
             $iframe.trigger('resize.view');
           }, 750);
         });
 
-        $iframeContainer.css({
+        $target.css({
           width: dataResizer
         });
 
