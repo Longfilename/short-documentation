d9e18ffc00310297d9d1cecef5b88581abf2db80
CelerityTim <tstephens@celerity.com>
2016-11-03 13:02:04 -0400

    removed underscore from filenames.

--- app/templates/src/docs/js/docs.js
+++ app/templates/src/docs/js/docs.js
@@ -1,7 +1,7 @@
 // load components;
-import '../components/component-view/_component-view';
-import '../components/nav/_nav';
-import '../components/perspective/_perspective';
-import '../components/resizer/_resizer';
-import '../components/tabs/_tabs';
-import '../components/template/_template';
+import '../components/component-view/component-view';
+import '../components/nav/nav';
+import '../components/perspective/perspective';
+import '../components/resizer/resizer';
+import '../components/tabs/tabs';
+import '../components/template/template';
