27556338e6e140165afb161c26ccda68e3a41e36
CelerityTim <tstephens@celerity.com>
2016-11-03 14:16:31 -0400

    perspective => page.
    removed scrollbar once template view is open.

--- app/templates/src/docs/components/page/page.js
+++ app/templates/src/docs/components/page/page.js
@@ -1,16 +1,15 @@
 // cf. http://tympanus.net/Development/PerspectivePageViewNavigation/index4.html
 
-const $window = $(window);
-const $perspective = $('.perspective');
-const $content = $('.perspective__content');
-const click = 'touchstart.perspective click.perspective';
-const transitionEnd = 'transitionend.perspective';
-const animateClass = 'perspective--animate';
-const modalClass = 'perspective--modalview';
+const $page = $('.page');
+const $frame = $('.page__frame');
+const click = 'touchstart.page click.page';
+const transitionEnd = 'transitionend.page';
+const animateClass = 'page--animate';
+const modalClass = 'page--modalview';
 
 // init open button events;
 // close events will be set AFTER open events have fired;
-$('.js__perspective').on(click, showMenu);
+$('.js__page').on(click, showMenu);
 
 //
 
@@ -18,28 +17,25 @@ function showMenu (event) {
   // prevent a click from firing on the $wrapper (and closing the modal);
   event.stopPropagation();
 
-  // scroll to the top of the page so we can push the content down and show the navigation;
-  $window.scrollTop(0);
-
   // show the thumbnail view and navigation;
   // get the content ready with the modal class (nothing visually changes yet);
-  $perspective.addClass(modalClass);
+  $page.addClass(modalClass);
 
   // move the content into the modal view;
-  $perspective.addClass(animateClass);
+  $page.addClass(animateClass);
 
   // now configure the close event handler;
   // if this was done on page load - the transition end listener would be configured before we want it to be;
-  $content.one(click, hideMenu);
+  $frame.one(click, hideMenu);
 }
 
 function hideMenu (event) {
   // after the transition has finished running;
-  $perspective.one(transitionEnd, () => {
+  $page.one(transitionEnd, () => {
     // remove the modal class (so the content isn't fixed anymore);
-    $perspective.removeClass(modalClass);
+    $page.removeClass(modalClass);
   });
 
   // start the process of sending the modal view back to the default desktop view;
-  $perspective.removeClass(animateClass);
+  $page.removeClass(animateClass);
 }
--- app/templates/src/docs/js/docs.js
+++ app/templates/src/docs/js/docs.js
@@ -1,7 +1,7 @@
 // load components;
 import '../components/component-view/component-view';
 import '../components/nav/nav';
-import '../components/perspective/perspective';
+import '../components/page/page';
 import '../components/resizer/resizer';
 import '../components/tabs/tabs';
 import '../components/template/template';
