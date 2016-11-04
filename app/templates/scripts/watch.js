3f75e44c3b1cd266588d6089a02fbe058a834b45
CelerityTim <tstephens@celerity.com>
2016-11-04 16:58:36 -0400

    fixed watcher to properly compile docs.

--- app/templates/scripts/watch.js
+++ app/templates/scripts/watch.js
@@ -1,44 +1,61 @@
 const chokidar = require('chokidar'); // watches for file changes;
 const scss = require('./scss');
 const ts = require('./ts');
-const copy = require('./copy');
+const images = require('./images');
 const htmlTemplates = require('./html--templates');
 const htmlIndex = require('./html--index');
 const docs = require('./docs/docs');
-
-const config = {
+const watchConfig = {
   images: {
-    path: 'src/images/**/*',
-    command: function () {
-      copy('src/images', 'dist/images');
-    }
+    path: 'src/components/**/images/*',
+    command: images
   },
   scss: {
-    path: 'src/**/*.scss',
-    command: scss,
-    docs: docs.scss
+    path: ['src/components/**/*.scss', 'src/scss/**/*.scss'],
+    command: scss
   },
   ts: {
-    path: ['src/**/*.ts'],
-    command: ts,
-    docs: docs.js
+    path: ['src/components/**/*.ts', 'src/ts/**/*.ts'],
+    command: ts
   },
   html: {
-    path: ['src/**/*.jade', 'src/**/*.json'],
-    command: function () {
+    path: ['src/components/**/*.jade', 'src/templates/**/*.jade', 'src/components/**/*.json'],
+    command: function (startDocs) {
       htmlTemplates();
       htmlIndex();
-    },
-    docs: docs.html
+
+      // if docs are running, rerun docs HTML because the site HTML changed;
+      if (startDocs) {
+        docs.html();
+      }
+    }
   }
 };
 
 module.exports = function (startDocs) {
-  // configure watcher for each filetype;
-  // each file type is a property in the config object;
-  // so looping through each property will start a watcher for each file type;
-  Object.getOwnPropertyNames(config).forEach((val, idx, array) => {
-    const watchObj = config[val];
+  // if the docs are running, watch those files too;
+  if (startDocs) {
+    watchConfig.docsScss = {
+      path: 'src/docs/**/*.scss',
+      command: docs.scss
+    };
+    watchConfig.docsTs = {
+      path: 'src/docs/**/*.ts',
+      command: docs.ts
+    };
+    watchConfig.docsHtml = {
+      path: 'src/docs/**/*.jade',
+      command: docs.html
+    };
+    watchConfig.docsReadme = {
+      path: 'src/**/README.md',
+      command: docs.html
+    };
+  }
+
+  // configure watcher for each entry in the config;
+  Object.getOwnPropertyNames(watchConfig).forEach((val, idx, array) => {
+    const watchObj = watchConfig[val];
     const colors = require('colors'); // pretty console output;
 
     // start the watcher, one watcher per property (html, scss, js, images);
@@ -51,13 +68,11 @@ module.exports = function (startDocs) {
         // report on the changed file;
         console.log(filename.blue, 'changed, rebuilding.');
 
-        watchObj.command();
-
-        if (startDocs && watchObj.docs) {
-          watchObj.docs();
-        }
+        // run the rebuilding command;
+        watchObj.command(startDocs);
       });
 
+    // tell the world what is going on;
     console.log(val.blue, 'watcher has been started.');
   });
 };
