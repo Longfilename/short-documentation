1b6f3da5f59090c5d3633b0960c0ec84e2a2c231
CelerityTim <tstephens@celerity.com>
2016-11-05 15:43:37 -0400

    comments.

--- app/templates/scripts/util.js
+++ app/templates/scripts/util.js
@@ -6,8 +6,6 @@ module.exports = {
   // a few tasks require the destination folder to be already present;
   // this function will take an array, representing a path, and create the folders;
   makeFolders: (pathArray) => {
-    // if there is no path being passed in, then it's probably a watcher;
-    // if it is a watcher, there is no folder that needs to be created;
     if (pathArray.length) {
       let folder = '';
       let createFolder = true;
