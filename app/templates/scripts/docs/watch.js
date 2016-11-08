d77b4b7c2db068270b5f8db5aa6ccb7cf4de60bd
CelerityTim <tstephens@celerity.com>
2016-11-08 11:29:55 -0500

    added all jade files to documentation watcher.

--- app/templates/scripts/docs/watch.js
+++ app/templates/scripts/docs/watch.js
@@ -1,23 +1,23 @@
+const util = require('../_util'); // Short Documentation shared functions;
+
+// import documentation build scripts;
 const scss = require('./scss');
 const ts = require('./ts');
 const html = require('./html');
-const util = require('../_util'); // Short Documentation shared functions;
-
-const watchConfig = {
-  scss: {
-    path: 'src/docs/**/*.scss',
-    command: scss
-  },
-  ts: {
-    path: 'src/docs/**/*.ts',
-    command: ts
-  },
-  html: {
-    path: ['src/docs/**/*.jade', 'src/components/**/*.json', 'src/**/README.md'],
-    command: html
-  }
-};
 
 module.exports = () => {
-  util.watch(watchConfig);
+  util.watch({
+    scss: {
+      path: 'src/docs/**/*.scss',
+      command: scss
+    },
+    ts: {
+      path: 'src/docs/**/*.ts',
+      command: ts
+    },
+    html: {
+      path: ['src/**/*.jade', 'src/**/*.json', 'src/**/README.md'],
+      command: html
+    }
+  });
 };
